41a56734dbea51c3ff8907620ebafca81d7f7bb6
Ananth Narayan <narayanananth26@gmail.com>
2023-06-10 12:29:52 +0530

    conditionally render comps

--- src/index.js
+++ src/index.js
@@ -67,14 +67,24 @@ function Header() {
 }
 
 function Menu() {
+	const pizzas = pizzaData;
+	// const pizzas = [];
+	const numPizzas = pizzas.length;
+
 	return (
 		<main className="menu">
 			<h2>Our Menu</h2>
-			<ul className="pizzas">
-				{pizzaData.map((pizza) => (
-					<Pizza pizzaObj={pizza} key={pizza.name} />
-				))}
-			</ul>
+			{numPizzas > 0 ? (
+				<ul className="pizzas">
+					{pizzaData.map((pizza) => (
+						<Pizza pizzaObj={pizza} key={pizza.name} />
+					))}
+				</ul>
+			) : (
+				<p>
+					We're still working on our menu. Please come back later :)
+				</p>
+			)}
 			{/* <Pizza
 				name="Pizza Spinacci"
 				ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
@@ -106,7 +116,20 @@ function Footer() {
 
 	return (
 		<footer className="footer">
-			{new Date().toLocaleTimeString()}. We're currently open
+			{isOpen ? (
+				<div className="order">
+					<p>
+						We're open until {closeHour}:00. Come visit us or order
+						online.
+					</p>
+					<button className="btn">Order</button>
+				</div>
+			) : (
+				<p>
+					We're happy to welcome you between {openHour}:00 and{" "}
+					{closeHour}:00 .
+				</p>
+			)}
 		</footer>
 	);
 }
